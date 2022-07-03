341b6b194132c6c80a676d91bb32b9cf57588843
Sumith Damodaran <sumithpd@gmail.com>
2022-07-03 15:55:41 +0100

    Components and passing events

---
+++ FirstBlazorApp/ItemModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstBlazorApp
+{
+    public class ItemModel
+    {
+        public string ItemId { get; set; }
+        public double AskingPrice { get; set; }
+        public double Units { get; set; }
+        public double Total => Units * AskingPrice;
+
+
+    }
+}
---
+++ FirstBlazorApp/PersonModel.cs
@@ -0,0 +1,10 @@
+﻿ namespace FirstBlazorApp
+{
+    public class PersonModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Gender { get; set; }
+    }
+}
